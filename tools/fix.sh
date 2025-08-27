ad4c403e0b81d17894196bda425d1861364ae5b4
dymaczew <dymaczew@gmail.com>
2025-08-27 17:42:27 +0200

    added fix.sh script for 2.0.0.1 install on a VM

---
+++ tools/fix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+json_file="$HOME/ibm-concert/concert-hub/localstorage/volumes/infra/account-instances/0000-0000-0000-0000.json"
+if [ -e $json_file ]; then
+        sed -i ':a;N;$!ba;s/\n/ /g' $json_file
+        contents="$(jq '.[1].instance_url = "https://concert:12443"' $json_file)" && echo -E "${contents}" > $json_file
+else
+        echo "File $json_file not found"
+fi
