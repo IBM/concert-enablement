030594b6b9710bfacaa6f75b20fc5a406fdec4b1
dymaczew <dymaczew@gmail.com>
2025-08-28 16:13:42 +0200

    changed port to 443

--- tools/fix.sh
+++ tools/fix.sh
@@ -2,7 +2,7 @@
 json_file="$HOME/ibm-concert/concert-hub/localstorage/volumes/infra/account-instances/0000-0000-0000-0000.json"
 if [ -e $json_file ]; then
         sed -i ':a;N;$!ba;s/\n/ /g' $json_file
-        contents="$(jq '.[1].instance_url = "https://concert:12443"' $json_file)" && echo -E "${contents}" > $json_file
+        contents="$(jq '.[1].instance_url = "https://concert:443"' $json_file)" && echo -E "${contents}" > $json_file
 else
         echo "File $json_file not found"
 fi
